ead2ecec1063a730fb8fb35f10ead5e5abee34b0
Mostafa Elkaranshawy <110842052+MostafaElKaranshawy@users.noreply.github.com>
2022-12-23 16:19:21 +0200

    Update connect 4.c

--- connect 4.c
+++ connect 4.c
@@ -11,6 +11,7 @@ typedef struct {
     int score;
     int moves ;
     char name[255];
+    int time;
 }players;
 
 players player_1 , player_2 ;
@@ -57,51 +58,69 @@ int main()
         }
     }
     while(1){
-        main_menu(a,arri,arrj);
+        if(getch() == '\r'){
+            main_menu(a,arri,arrj);
+        }
     }
 
 
 }
 void main_menu(int a[][width+3],int arri[],int arrj[]){
 
-    if(getch() == '\r'){
+    // if(getch() == '\r'){
         printf("Hello To Connect 4!\n");
         printf("\nMain Menu\n");
         printf("\nNew Game 'Press N' \n");
+        printf("\nTop Players 'Press T'\n");
         printf("\nLoad Game 'Press L' \n");
         printf("\nQuit Game 'Press Q' \n");
-    }
-    fflush(stdin);
-    switch(getch()){
-        case 'n':
-            system("cls");
-            new_game();
-            system("cls");
-            print(a);
-            while(counter < width * height){
-                input(a,arri,arrj);
-            }
-            exit(1);
-            break;
-        case 'l':
-            system("cls");
-            printf("Loading File ...\n");
-            break;
-        case 'q':
-            system("cls");
-            printf("Closing the Game....\nBye...!\n");
-            exit(1);
-            break;
-            //default here plz
-    }
+        fflush(stdin);
+        switch(getch()){
+            case 'n':
+                system("cls");
+                new_game();
+                system("cls");
+                print(a);
+                while(counter < width * height){
+                    input(a,arri,arrj);
+                }
+                exit(1);
+                break;
+            case 'l':
+                system("cls");
+                printf("Loading File ...\n");
+                break;
+            case 'q':
+                system("cls");
+                printf("Quit Game.....!\n");
+                printf("\nYes 'y'\tNO 'n'\n");
+                char sure = getch();
+                if(sure == 'y'){
+                    printf("BYE.............!");
+                    exit(1);
+                }
+                else if(sure == 'n'){
+                    system("cls");
+                    main_menu(a,arri,arri);
+                    }
+                break;
+            default:
+                system("cls");
+                main_menu(a,arri,arrj);
+                break;
+        }
+    // }
+
 }
 
 void input(int a[][width+3],int arri[],int arrj[]){
     // print(a);
     int human=1;
-    printf("Player 1 Moves: %d\t%s Moves: %d\n",player_1.moves,player_2.name, player_2.moves);
-    printf("================================================================\n");
-    printf("\nplayer 1 score: %d\tplayer 2 score: %d\n ",player_1.score,player_2.score);
+    printf("\n");
+    printf("\033[0;31mPlayer 1 Moves: %d\033[0m\t\033[0;34m%s Moves: %d\033[0m\n",player_1.moves,player_2.name, player_2.moves);
+    printf("\n\033[0;31mplayer 1 score: %d\033[0m\t\033[0;34mplayer 2 score: %d\033[0m\n ",player_1.score,player_2.score);
+    printf("");
+
     char l;
     int qqq;
     p_players(turn%2+1);
@@ -118,27 +137,27 @@ void input(int a[][width+3],int arri[],int arrj[]){
         printf("\nEnter your move: ");
         scanf("%c", &l);
         printf("\n");
-        qqq=l-'0';
+        qqq=l -'0';
     }
     else if(!(strcmp(player_2.name,"Player 2"))){
         printf("\nEnter your move: ");
         scanf("%c", &l);
         printf("\n");
-        qqq=l-'0';
+        qqq=l -'0';
     }
-    if(l=='u'||l=='d'){
+    // if(l=='u'||l=='d'){
+    //     gravity(a,l,arri,arrj,human);
+    // }
+    if(l=='q' ||l == 'm' || l == 's' || l == 'u'||l == 'd' || (qqq <= width && qqq > 0)){
         gravity(a,l,arri,arrj,human);
     }
-
-   else if(!(qqq<= width && qqq > 0)){
+    else if (!(l=='q' ||l == 'm' || l == 's' || l == 'u'||l == 'd' || (qqq <= width && qqq > 0))){
         system("cls");
         printf("\n No Available Place Here!\n Try Another Place\n");
         print(a);
         input(a,arri,arrj);
     }
-    else{
-        gravity(a,l,arri,arrj,human);
-    }
+
     system("cls");
     print(a);
     // p_players(turn%2+1);
@@ -146,70 +165,94 @@ void input(int a[][width+3],int arri[],int arrj[]){
 }
 
 void gravity(int a[][width+3],char qqq,int arri[],int arrj[],int human){
-    int f=qqq-'0';
+    int f=qqq - '0';
     int arrscore1[width*height],arrscore2[width*height];
     char symbol[2] = {player_1.symbol , player_2.symbol};
-    if(qqq!='u'&&qqq!='d'){
-    for(int i =height-1; i>=0; i--){
-        if(i == height-1 && a[i][f-1] ==' '){           // first row at the bottom game.
+    if(f < width && f >= 0){
+        for(int i =height-1; i>=0; i--){
+            if(i == height-1 && a[i][f-1] ==' '){           // first row at the bottom game.
 
-            a[i][f-1] = symbol[turn%2] ;
-            arri[counter]=i;
-            arrj[counter]=f-1;
+                a[i][f-1] = symbol[turn%2] ;
+                arri[counter]=i;
+                arrj[counter]=f-1;
 
-
-             //arrscore1[counter]=0;
-           // arrscore2[counter]=0;
-             check_total(a,i,f-1);
-            if(turn%2 == 0){player_1.moves++;}
-            if(turn%2 == 1){player_2.moves++;}
-            turn++;
-            counter++;
-            break;
-        }
-        else if ( a[i][f-1] ==' ' && a[i+1][f-1] !=' '){
-            a[i][f-1] = symbol[turn%2] ;
-            arri[counter]=i;
-            arrj[counter]=f-1;
-    //         arrscore1[counter]=0;
-      //      arrscore2[counter]=0;
-        //    arrscore1[counter]=player_1.score;
-          //  arrscore2[counter]=player_2.score;
-            check_total(a,i,f-1);
-            if(turn%2 == 0){player_1.moves++;}
-            if(turn%2 == 1){player_2.moves++;}
-            turn++;
-            counter++;
-            break;
-        }
-        else {
-            if (i == 0)
-            {
-                input(a,arri,arri);
-                printf("\nNo Available Place Here!\n Try Another Place\n");
+                check_total(a,i,f-1);
+                if(turn%2 == 0){player_1.moves++;}
+                if(turn%2 == 1){player_2.moves++;}
+                turn++;
+                counter++;
+                break;
+            }
+            else if ( a[i][f-1] ==' ' && a[i+1][f-1] !=' '){
+                a[i][f-1] = symbol[turn%2] ;
+                arri[counter]=i;
+                arrj[counter]=f-1;
+                check_total(a,i,f-1);
+                if(turn%2 == 0){player_1.moves++;}
+                if(turn%2 == 1){player_2.moves++;}
+                turn++;
+                counter++;
+                break;
+            }
+            else {
+                if (i == 0)
+                {
+                    input(a,arri,arri);
+                    printf("\nNo Available Place Here!\n Try Another Place\n");
+                }
             }
         }
-    }}
-    else if(qqq=='u'){
-
-    undo(a,arri,arrj);
     }
-else if(qqq=='d'){
-     /*   int x;
-    x=arri[counter];
-            int d;
-            d=arrj[counter];*/
+    else if(qqq == 'q'){
+        system("cls");
+        printf("\n\n\nQuit Game.....!\n\n\n");
+        printf("\t\tYes 'y'\t\tNO 'n'\n");
+        char sure = getch();
+        if(sure == 'y'){
+            system("cls");
+            printf("\n\n\n\nBYE.............!");
+            exit(1);
+        }
+        else if(sure == 'n'){
+            system("cls");
+            input(a,arri,arri);
+            }
+    }
+    else if(qqq == 'm'){
+        system("cls");
+        printf("\n\n\nReurn to main menu ?\n\n\n\tYes 'y'\t\tNO 'n'\n");
+        char back = getch();
+        if(back == 'y'){
+            printf("\n\n\nreturning to main menu.....\n\n\n");
+            sleep(1);
+            system("cls");
+            main_menu(a,arri,arrj);            
+        }
+        if(back == "n"){
+            system("cls");
+            input(a,arri,arri);
+        }
+
+    }
+    else if(qqq == 's'){
+        system("cls");
+        printf("\tSaving Game.....\n");
+        sleep(1);
+        printf("\n\n\tGame Saved\n");
+        sleep(1);
+        system("cls");
+        input(a,arri,arri);
+    }
+    else if(qqq=='u'){
+        undo(a,arri,arrj);
+    }
+    else if(qqq=='d'){
         redo(a,arri,arrj);
-}
+    }
 }
 
 void print(int a[][width+3]){
-    printf("\n");
-    // for(int sss =1; sss<=width ; sss++){
-        // if(sss == 1){printf("%d ",sss);}
-        // else
-        // printf(" %d ",sss);
-    // }
+
     printf("\n ");
     for(int s = 0 ;s<width ; s++){
         printf("----");
@@ -265,27 +308,29 @@ void print(int a[][width+3]){
         }
         printf("\n");
     }
-    printf("PRESS 'U' FOR <-  || PRESS 'D' FOR ->\n");
+    printf("PRESS 'u' FOR UNDO\t\t\t PRESS 'd' FOR REDO\n");
 
 }
 
 void p_players(int qq){
     if(!(strcmp(player_2.name , "computer"))){
         if(qq == 2){
-            printf("\n\nComputer Turn!\n\n");
+            printf("\n\n\t\t\t\033[0mComputer Turn!\033[0m\n\n");
             // player_1.moves++;
         }
         else if( qq == 1){
-            printf("\n\nPlayer [1] Turn!\n\n");
+            printf("\n\n\033[0;31mPlayer [1] Turn!\n\n");
             // player_2.moves++;
         }
     }
     else{
-        printf("\n\nPlayer [%d] Turn!\n\n",qq);
-        // if(qq == 1){player_2.moves++;}
-        // else if(qq == 2){player_1.moves++;}
+        if(qq %2 == 1){
+            printf("\n\n\033[0;31mPlayer [1] Turn!\033[0m\n\n");
+        }
+        else if(qq %2 == 0){
+            printf("\n\n\t\t\t\033[0;34mPlayer [2] Turn!\033[0m\n\n");
+        }
     }
-
 }
 
 void new_game(){
@@ -298,6 +343,10 @@ void new_game(){
             break;
         case 'c':
             strcpy(player_2.name , "computer");
+            break;
+        default:
+            system("cls");
+            new_game();
             break;
     }
 }
@@ -343,8 +392,8 @@ if(a[i][j]==player_1.symbol){
         else if(a[i][j]==player_2.symbol){
              player_2.score++;
         }
-
 }
+
 void check_diagonalr(int a[][width+3],int i,int j){
 if(a[i][j]==a[i+1][j+1]&&a[i+1][j+1]==a[i+2][j+2]&&a[i+2][j+2]==a[i+3][j+3]){
    if(a[i][j]==player_1.symbol){
@@ -377,9 +426,9 @@ if(a[i][j]==player_1.symbol){
         else if(a[i][j]==player_2.symbol){
              player_2.score++;
         }
-
-
 }
+
+
 void check_diagnall(int a[][width+3],int i,int j){
 if(a[i][j]==a[i-1][j+1]&&a[i-1][j+1]==a[i-2][j+2]&&a[i-2][j+2]==a[i-3][j+3]){
    if(a[i][j]==player_1.symbol){
@@ -412,66 +461,65 @@ if(a[i][j]==player_1.symbol){
         else if(a[i][j]==player_2.symbol){
             player_2.score++;
         }
-
-
 }
+
+
 void check_total(int a[][width+3],int i,int j){
 
- check_colum(a,i,j);
- check_row(a,i,j);
- check_diagonalr(a,i,j);
-check_diagnall(a,i,j);
+    check_colum(a,i,j);
+    check_row(a,i,j);
+    check_diagonalr(a,i,j);
+    check_diagnall(a,i,j);
 
 }
 void undo(int a[][width+3],int i[], int j[]){
-int h,k,y,z;
-h=i[counter-1];
-k=i[counter-2];
-y=j[counter-1];
-z=j[counter-2];
-if(!(strcmp(player_2.name,"Player 2"))){
-a[h][y]=' ';
-print(a);
-counter--;
-turn--;
-if(turn%2 == 0){player_1.moves--;}
-else if(turn%2 == 1){player_2.moves--;}}
- else if(!(strcmp(player_2.name,"computer"))){
-    a[h][y]=' ';
-    a[k][z]=' ';
-print(a);
-counter=counter-2;
+    int h,k,y,z;
+    h=i[counter-1];
+    k=i[counter-2];
+    y=j[counter-1];
+    z=j[counter-2];
+    if(!(strcmp(player_2.name,"Player 2"))){
+        a[h][y]=' ';
+        print(a);
+        counter--;
+        turn--;
+        if(turn%2 == 0){player_1.moves--;}
+        else if(turn%2 == 1){player_2.moves--;}}
+    else if(!(strcmp(player_2.name,"computer"))){
+        a[h][y]=' ';
+        a[k][z]=' ';
+        print(a);
+        counter=counter-2;
 
-turn=turn-2;
-player_2.moves--;
-player_1.moves--;
- }
-
+        turn=turn-2;
+        player_2.moves--;
+        player_1.moves--;
+    }
 }
 void redo(int a[][width+3],int i[], int j[]){
     int k,h,y,z;
     h=i[counter];
-k=i[counter+1];
-y=j[counter];
-z=j[counter+1];
- char symbol[2] = {player_1.symbol , player_2.symbol};
-if(!(strcmp(player_2.name,"Player 2"))){
- a[h][y]=symbol[turn%2];
-print(a);
-counter++;
-turn++;
-if(turn%2 == 1){player_1.moves++;}
-if(turn%2 == 0){player_2.moves++;}
-}
-else if(!(strcmp(player_2.name,"computer"))){
-    a[h][y]=player_1.symbol;
-    turn++;
-    a[k][z]=player_2.symbol;
-    counter=counter+2;
-      turn++;
-    player_1.moves++;
-    player_2.moves++;
-}
+    k=i[counter+1];
+    y=j[counter];
+    z=j[counter+1];
+    char symbol[2] = {player_1.symbol , player_2.symbol};
+    if(!(strcmp(player_2.name,"Player 2"))){
+        a[h][y]=symbol[turn%2];
+        print(a);
+        counter++;
+        turn++;
+        if(turn%2 == 1){player_1.moves++;}
+        if(turn%2 == 0){player_2.moves++;}
+    }
+    else if(!(strcmp(player_2.name,"computer"))){
+        a[h][y]=player_1.symbol;
+        turn++;
+        a[k][z]=player_2.symbol;
+        counter=counter+2;
+        turn++;
+        player_1.moves++;
+        player_2.moves++;
+    }
 }
 void save(){}
 void load(){}
