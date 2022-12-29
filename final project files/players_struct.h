2dab703e7c0ad746d1694d4624241560d9ecc2d5
Mostafa Elkaranshawy <110842052+MostafaElKaranshawy@users.noreply.github.com>
2022-12-29 17:41:13 +0200

    Update players_struct.h

--- final project files/players_struct.h
+++ final project files/players_struct.h
@@ -6,3 +6,14 @@ typedef struct {
     char name[255];
 }players;
 players player_1 , player_2 ;
+
+typedef struct{
+    int a1[6][7];
+    int counting;
+    int seconds;
+    int score[2];
+    int moves[2];
+    char name[20];
+    int countep;
+}save_game;
+save_game savess;
